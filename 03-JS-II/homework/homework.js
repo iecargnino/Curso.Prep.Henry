d1ab6ef652f3350e353a0ecad8f2e4d13f7d063e
Ema Cargnino <iecargnino@gmail.com>
2021-01-11 13:21:34 -0300

    Ejercicos II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -62,7 +62,7 @@ function fizzBuzz(numero) {
   if (numero % 15 === 0) return "fizzbuzz";
     if (numero % 3 === 0) return "fizz";
       if (numero % 5 === 0) return "buzz";
-      return numero;
+        return numero;
 }
 
 function esPrimo(numero) {
